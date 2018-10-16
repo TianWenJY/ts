72cb8d0dd6ed3f192b3055772e0c8a9a428da069
wangkai <l>
2018-10-16 18:17:53 +0800

    修改部分功能

--- myCheshili/src/page/index/index.vue
+++ myCheshili/src/page/index/index.vue
@@ -12,27 +12,33 @@
         text-color="#fff"
         active-text-color="#ffd04b">
         <el-submenu index="2">
-          <el-menu-item index="index">
+<!--           <el-menu-item index="4">
             <i class="el-icon-setting"></i>
             <span slot="title">统计</span>
-          </el-menu-item>
+          </el-menu-item> -->
           <template slot="title">
             <i class="el-icon-location"></i>
-            <span>导航一</span>
+            <span>通联支付</span>
           </template>
-          <el-menu-item-group>
+          <el-menu-item index="areacode">地区码列表</el-menu-item>
+          <el-menu-item index="firstInfo">用户银行卡</el-menu-item>
+          <el-menu-item index="firstInfo">商户卡列表</el-menu-item>
+          <el-menu-item index="firstInfo">订单号列表</el-menu-item>
+          <el-menu-item index="firstInfo">交易记录</el-menu-item>
+          <el-menu-item index="tluser">商户列表</el-menu-item>
+<!--           <el-menu-item-group>
             <template slot="title">团队管理</template>
             <el-menu-item index="firstInfo">代理商列表</el-menu-item>
             <el-menu-item index="1-2">选项2</el-menu-item>
           </el-menu-item-group>
           <el-menu-item-group title="分组2">
             <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="4">
+          </el-menu-item-group> -->
+<!--           <el-submenu index="4">
             <template slot="title">进出场记录</template>
             <el-menu-item index="engine">进场记录</el-menu-item>
             <el-menu-item index="engine">出场记录</el-menu-item>
-          </el-submenu>
+          </el-submenu> -->
         </el-submenu>
         <el-menu-item index="engine">
           <i class="el-icon-menu"></i>
@@ -52,8 +58,7 @@
         </el-submenu>
       </el-menu>
       </el-aside>
-        <el-col>
-        <head-top></head-top>
+        <el-col style="overflow-y:auto;    overflow-x: hidden;">
         <keep-alive>
           <router-view></router-view>
         </keep-alive>
--- myCheshili/src/router/index.js
+++ myCheshili/src/router/index.js
@@ -14,6 +14,9 @@ import setting from '../page/setting/setting.vue'
 import firstInfo from '../page/firstInfo.vue'
 import common from '../common/common.css'
 import commonJs from '../common/common.js'
+import AreaCode from '../page/areacodeList.vue'
+import TlUser from '../page/TLUser.vue'
+import textFather from '../page/textFather.vue'
 Vue.use(VueResource);
 Vue.use(ElementUI);
 Vue.use(Router);
@@ -35,7 +38,7 @@ export default new Router({
       children: [{
         path: '',
         component: home,
-        meta: [],
+        meta: ['进出车记录', '进车记录'],
       },{
       path: '/engine',
       component: engine,
@@ -56,6 +59,18 @@ export default new Router({
         path: '/firstInfo',
         component: firstInfo,
         meta: ['团队管理','一级代理商']
+      },{
+        path: '/areacode',
+        component: AreaCode,
+        meta: ['通联支付','通联地区码列表']
+      },{
+        path: '/tluser',
+        component: TlUser,
+        meta: ['通联支付','商户列表']
+      },{
+        path: '/textFather',
+        component: textFather,
+        meta: ['通联支付','商户列表']
       }
     ]
     }
